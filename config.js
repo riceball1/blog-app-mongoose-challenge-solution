680070a274e505ff7539404446b8ca5f255df908
Dana Ng <ngdana1@gmail.com>
2017-01-24 11:38:07 -0800

    removed database url in config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                      'mongodb://localhost/blog-app';
-exports.TEST_DATABASE_URL = (process.env.TEST_DATABASE_URL ||
-	'mongodb://localhost/test-restaurants-app');
+                       global.DATABASE_URL 
+                      //  ||
+                      // 'mongodb://localhost/blog-app-mongoose-challenge-solution';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
 exports.PORT = process.env.PORT || 8080;
